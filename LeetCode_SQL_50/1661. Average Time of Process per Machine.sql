c6145a359e0d38937bf657a1264c685c81706a66
JcbNesh <babayqb13@gmail.com>
2026-04-08 22:38:16 +0400

    Nothing

--- LeetCode_SQL_50/1661. Average Time of Process per Machine.sql
+++ LeetCode_SQL_50/1661. Average Time of Process per Machine.sql
@@ -15,6 +15,6 @@ on a.machine_id = b.machine_id
    and a.activity_type = 'start'
    and b.activity_type = 'end'
  group by a.machine_id;
-
+  
 /* Explanation: This query calculates the average processing time for each machine by joining the activity table with itself to match start and end activities for the same machine and process. It then groups the results by machine_id and rounds the average processing time to three decimal places.
  */
